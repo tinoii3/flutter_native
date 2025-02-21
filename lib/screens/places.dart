609d10e2f850ad808c21e65d4fa52948acdf39fb
tinoii3 <kritsanai.bun@gmail.com>
2025-02-21 13:47:52 +0700

    add file

---
+++ lib/screens/places.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native/providers/user_places.dart';
+import 'package:flutter_native/screens/add_place.dart';
+import 'package:flutter_native/widgets/places_list.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: userPlaces,
+      ),
+    );
+  }
+}
